710d7284cc6be926ba6736ba135f0d50c6823630
jona0001 <jona0001@stud.kea.dk>
2024-03-04 18:46:38 +0100

    added classes, Main, Room, Adventure, UserInterface.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        UserInterface userInterface = new UserInterface();
+        userInterface.navigate();
     }
 }
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,11 +1,44 @@
-public class UserInterface {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class UserInterface {
+    private Adventure adventure;
+    Scanner scanner = new Scanner(System.in);
+
+    public UserInterface() {
+        adventure = new Adventure();
+        scanner.useDelimiter("\n");
+    }
+
+    public void startProgram() {
+        int sentinel = 9;
+
+
+        while (menuChoice != sentinel){
+
+        }
     }
 
 
+    public void navigate() {
+        String input = scanner.nextLine();
+        switch(input){
+            case "go north":
+                System.out.println("Go north");
+                break;
+            case "go south":
+                System.out.println("Go south");
+                break;
+            case "go west":
+                System.out.println("Go west");
+                break;
+            case "go east":
+                System.out.println("Go east");
+                break;
+        }
 
-    public void MenuText() {
+    }
+
+    public void menuText() {
         System.out.println("***** Menu *****");
         System.out.println("1. look around");
         System.out.println("2: go north");
