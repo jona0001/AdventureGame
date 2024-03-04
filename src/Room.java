dbb1bf28b861cf31672e9ed7a00c03b89f25e7ed
jona0001 <jona0001@stud.kea.dk>
2024-03-04 11:13:27 +0100

    added classes, Main, Room, Adventure, UserInterface.

--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,24 @@
 public class Room {
+    private String name;
+    private String description;
+    private Room north;
+    private Room south;
+    private Room east;
+    private Room west;
+
+    public void setNorth(Room north){
+        this.north = north;
+    }
+    public void setSouth(Room south){
+        this.north = south;
+    }
+    public void setEast(Room east){
+        this.east = east;
+    }
+    public void setWest(Room west){
+        this.west = west;
+    }
+
+
+
 }
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,2 +1,19 @@
 public class UserInterface {
+    public static void main(String[] args) {
+
+    }
+
+
+
+    public void MenuText() {
+        System.out.println("***** Menu *****");
+        System.out.println("1. look around");
+        System.out.println("2: go north");
+        System.out.println("3: go east");
+        System.out.println("4: go South");
+        System.out.println("5: go West");
+        System.out.println("6: help");
+        System.out.println("7: Exit room");
+
+    }
 }
